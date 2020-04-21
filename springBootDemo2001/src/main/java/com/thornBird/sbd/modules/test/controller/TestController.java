6fa80bb3ca745f44ff44cb8531e0190d70e9dcec
8zy8 <64067544+8zy8@users.noreply.github.com>
2020-04-21 16:12:08 +0800

    "2020-04-20:Commit spring boot project"

---
+++ springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.thornBird.sbd.modules.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/test/appDesc")
+	@ResponseBody
+	public String getAppDesc(){
+		return "Hello world.";
+	}
+	
+}
