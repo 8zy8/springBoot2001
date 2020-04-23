cd058daad565ec0c67163ba937bb7074501a9314
asus <asus@DESKTOP-E0BC34V>
2020-04-23 17:34:52 +0800

    "fix bug"

--- mySpringBoot/src/main/java/com/thornBird/sbd/MySpringBootApplication.java
+++ mySpringBoot/src/main/java/com/thornBird/sbd/MySpringBootApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemo2001Application {
+public class MySpringBootApplication {
 
 	public static void main(String[] args) {
-		
-		SpringApplication.run(SpringBootDemo2001Application.class, args);
+		SpringApplication.run(MySpringBootApplication.class, args);
 	}
 
 }
